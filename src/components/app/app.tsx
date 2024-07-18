534420a65168e345b745bcc7bae0754ca755eca2
Mikhail Li <leemisha98@gmail.com>
2024-07-18 16:35:49 +0500

    feat: Add 404 Page, Routing Setup, and Private Route Component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,30 @@
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../private-route/private-route.tsx';
+
 import { DATA } from '../mock-data/const.ts';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
-    <MainPage offersData={DATA} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage offersData={DATA} />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/favorites" element={
+          <PrivateRoute>
+            <FavoritesPage />
+          </PrivateRoute>
+        }
+        />
+        <Route path="/offer/:id" element={<OfferPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
